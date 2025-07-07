f8b0ec681ea54b51cc8815e3587717035dd30b26
narendra7384 <narendra.yadav@live.com>
2025-07-08 00:55:37 +0530

    rg5 added (#4)

--- main.tf
+++ main.tf
@@ -15,6 +15,12 @@ resource "azurerm_resource_group" "rg3" {
     location = "East US"
 }
 
+  resource "azurerm_resource_group" "rg5" {
+    name     = "rg_narendra5"
+
+    location = "West US"
+}
+
 resource "azurerm_virtual_network" "vnet" {
     name                = "myVnet"
     address_space       = ["10.0.0.0/16"]
