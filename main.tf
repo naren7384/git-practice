14aba8909e7a72c40599346b264ec568c3a258f7
narendra7384 <narendra.yadav@live.com>
2025-07-08 01:15:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,12 @@ resource "azurerm_resource_group" "rg3" {
   location = "East US"
 }
 
+resource "azurerm_resource_group" "rg4" {
+    name     = "rg_narendra4"
+  location = "East US"
+}
+
+
   resource "azurerm_resource_group" "rg2" {
     name     = "rg_narendra2"
 
