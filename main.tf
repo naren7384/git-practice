9279fd5e46416cdcf0c7d28899d8bcfd53c31ea6
narendra7384 <narendra.yadav@live.com>
2025-07-13 23:14:58 +0530

    rg8 deleted (#9)
    doneeeee

--- main.tf
+++ main.tf
@@ -38,12 +38,10 @@ resource "azurerm_resource_group" "rg4" {
     location = "Central US"
 }
 
- resource "azurerm_resource_group" "rg8" {
-    name     = "rg_rakesh"
-
-    location = "West US"
-  }
-
+resource "azurerm_resource_group" "rg8" {
+    name     = "rg_narendra8"
+    location = "Central US"
+}
 resource "azurerm_virtual_network" "vnet" {
     name                = "myVnet"
     address_space       = ["10.0.0.0/16"]
